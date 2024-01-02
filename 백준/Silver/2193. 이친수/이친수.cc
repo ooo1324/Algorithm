91fae4a3113e80bb5196e459a25dbaf2c0443234
ooo1324 <50831015+ooo1324@users.noreply.github.com>
2024-01-02 21:56:46 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std; 
+long long d[91];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	d[1] = 1;
+	d[2] = 1;
+
+	for (int i = 3; i <= n; i++)
+	{
+		d[i] = d[i - 1] + d[i - 2];
+	}
+
+	cout << d[n];
+
+	return 0;
+}              
